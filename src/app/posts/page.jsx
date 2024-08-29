98c03e44d46c282c875b96f6782fef6ee18df08d
Paing Oo Maw <paingoomaw.edka@gmail.com>
2024-08-29 20:28:56 +0700

    getting ready

---
+++ src/app/posts/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+    return <main>posts</main>;
+};
+
+export default page;
---
+++ src/app/services/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <main className="flex items-center justify-center h-screen">
+            <h1 className="text-red-400">Home Page</h1>
+        </main>
+    );
+};
+
+export default page;
